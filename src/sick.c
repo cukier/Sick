8d11eb52699d308d307941b883b316d55cd99090
cuki2 <mauricio@krebs.com.br>
2017-02-02 14:38:26 -0200

    estrutura dsf60
    Signed-off-by: cuki2 <mauricio@krebs.com.br>

--- src/sick.c
+++ src/sick.c
@@ -6,8 +6,6 @@
  */
 
 #include "sick.h"
-#include <stdint.h>
-#include <stdbool.h>
 #include <stdlibm.h>
 
 #define T0_REG	55535
@@ -17,7 +15,6 @@ uint8_t *buffer;
 uint16_t buffer_index;
 
 #ifdef __PCH__
-//#use rs232(uart1, baud=115200, parity=E, stream=sl1)
 #use rs232(uart1, baud=115200, parity=E, stream=sl1)
 #endif
 
@@ -28,21 +25,8 @@ void DSF60_isr_rda() {
 	buffer_index++;
 
 #ifdef __PCH__
-//	set_timer0(0);
-//	setup_timer_0(T0_INTERNAL | T0_DIV_8);
 	clear_interrupt(INT_RDA);
 #endif
-}
-
-#ifdef __PCH__
-#INT_TIMER0
-#endif
-void DSF60_isr_tmr0() {
-#ifdef __PCH__
-	clear_interrupt(INT_TIMER0);
-	setup_timer_0(T0_OFF);
-#endif
-	t_ok = true;
 }
 
 void DSF60_send_request(uint8_t *req, uint16_t size) {
@@ -63,14 +47,12 @@ void DSF60_flush_buffer(void) {
 
 void DSF60_init_encoder(void) {
 #ifdef __PCH__
-	clear_interrupt(INT_TIMER0);
-	enable_interrupts(INT_TIMER0);
-
 	clear_interrupt(INT_RDA);
 	enable_interrupts(INT_RDA);
 #endif
 
 	DSF60_flush_buffer();
+	dsf60 = (dsf60_t *) malloc(sizeof(dsf60_t));
 
 	return;
 }
@@ -124,7 +106,68 @@ void DSF60_read_position(void) {
 	return;
 }
 
-uint8_t DSF60_read_encoder_type(void) {
+void DSF60_set_number_lines(uint32_t n_lines) {
+	uint8_t *req;
+
+	req = (uint8_t *) malloc(7 * sizeof(uint8_t));
+	req[0] = DSF60_ADDRESS;
+	req[1] = DSF60_COMMAND_SET_NUMBER_OF_LINES;
+	req[2] = (uint8_t) ((n_lines & 0x0F00) >> 16);
+	req[3] = (uint8_t) ((n_lines & 0x00F0) >> 8);
+	req[4] = (uint8_t) ((n_lines & 0x000F));
+	req[5] = 0x69; //ACCESS code
+	req[6] = DSF60_crc_sum(req, 6);
+	DSF60_send_request(req, 7);
+	free(req);
+
+	return;
+}
+
+void DSF60_set_electrical_interface(uint8_t mode) {
+	uint8_t *req;
+
+	req = (uint8_t *) malloc(5 * sizeof(uint8_t));
+	req[0] = DSF60_ADDRESS;
+	req[1] = DSF60_COMMAND_SET_ELECTRICAL_INTERFACE;
+	req[2] = mode;
+	req[3] = 0x69; //ACCESS code
+	req[4] = DSF60_crc_sum(req, 4);
+	DSF60_send_request(req, 5);
+	free(req);
+
+	return;
+}
+
+void DSF60_set_zero_pulse_width_electrical(uint8_t mode) {
+	uint8_t *req;
+
+	req = (uint8_t *) malloc(5 * sizeof(uint8_t));
+	req[0] = DSF60_ADDRESS;
+	req[1] = DSF60_COMMAND_SET_ZERO_PULSE_WIDTH_ELECTRICAL;
+	req[2] = mode;
+	req[3] = 0x69; //ACCESS code
+	req[4] = DSF60_crc_sum(req, 4);
+	DSF60_send_request(req, 5);
+	free(req);
+
+	return;
+}
+
+void DSF60_zero_set(void) {
+	uint8_t *req;
+
+	req = (uint8_t *) malloc(5 * sizeof(uint8_t));
+	req[0] = DSF60_ADDRESS;
+	req[1] = DSF60_COMMAND_ZERO_SET;
+	req[2] = 0x69; //ACCESS code
+	req[3] = DSF60_crc_sum(req, 3);
+	DSF60_send_request(req, 4);
+	free(req);
+
+	return;
+}
+
+void DSF60_read_encoder_type(void) {
 	uint8_t *req;
 
 	req = (uint8_t *) malloc(3 * sizeof(uint8_t));
@@ -134,8 +177,48 @@ uint8_t DSF60_read_encoder_type(void) {
 	DSF60_send_request(req, 3);
 	free(req);
 
-	return EXIT_SUCESS;
+	return;
+}
 
+void DSF60_set_zero_pulse_width_mechanical(uint16_t degrees) {
+	uint8_t *req;
+
+	req = (uint8_t *) malloc(3 * sizeof(uint8_t));
+	req[0] = DSF60_ADDRESS;
+	req[1] = DSF60_COMMAND_SET_ZERO_PULSE_WIDTH_MECHANICAL;
+	req[2] = (uint8_t) ((degrees & 0xF0) >> 8);
+	req[3] = (uint8_t) (degrees & 0x0F);
+	req[4] = DSF60_crc_sum(req, 4);
+	DSF60_send_request(req, 5);
+	free(req);
+
+	return;
+}
+
+void DSF60_read_zero_pulse_width_electrical_mechanical(void) {
+	uint8_t *req;
+
+	req = (uint8_t *) malloc(3 * sizeof(uint8_t));
+	req[0] = DSF60_ADDRESS;
+	req[1] = DSF60_COMMAND_READ_ZERO_PULSE_WIDTH_ELECTRIAL_MECHANICAL;
+	req[2] = DSF60_crc_sum(req, 2);
+	DSF60_send_request(req, 3);
+	free(req);
+
+	return;
+}
+
+void DSF60_read_electrical_interface(void) {
+	uint8_t *req;
+
+	req = (uint8_t *) malloc(3 * sizeof(uint8_t));
+	req[0] = DSF60_ADDRESS;
+	req[1] = DSF60_COMMAND_READ_ELECTRICAL_INTERFACE;
+	req[2] = DSF60_crc_sum(req, 2);
+	DSF60_send_request(req, 3);
+	free(req);
+
+	return;
 }
 
 void DSF60_disable_encoder(void) {
@@ -169,10 +252,13 @@ uint8_t DSF60_check(void) {
 	return EXIT_ERROR;
 }
 
-bool DSF60_make_transaction(DSF60_command_t command) {
-	uint8_t retries, resp;
+bool DSF60_make_transaction(DSF60_command_t command, uint32_t arg) {
+	uint8_t retries, resp, n, cont;
+	uint16_t pulse_width_resp;
 
 	retries = 200;
+	n = 0;
+	pulse_width_resp = 0;
 
 	do {
 		resp = EXIT_SUCESS;
@@ -190,10 +276,139 @@ bool DSF60_make_transaction(DSF60_command_t command) {
 	case DSF60_COMMAND_ENCODER_RESET:
 		DSF60_encoder_reset();
 		break;
+	case DSF60_COMMAND_READ_SERIAL_NUMBER:
+		DSF60_read_serial_number();
+		n = 30;
+		break;
+	case DSF60_COMMAND_READ_POSITION:
+		DSF60_read_position();
+		n = 7;
+		break;
+	case DSF60_COMMAND_SET_NUMBER_OF_LINES:
+		DSF60_set_number_lines(arg);
+		n = 3;
+		break;
+	case DSF60_COMMAND_SET_ELECTRICAL_INTERFACE:
+		DSF60_set_electrical_interface((uint8_t) arg);
+		n = 3;
+		break;
+	case DSF60_COMMAND_SET_ZERO_PULSE_WIDTH_ELECTRICAL:
+		DSF60_set_zero_pulse_width_electrical((uint8_t) arg);
+		n = 3;
+		break;
+	case DSF60_COMMAND_ZERO_SET:
+		DSF60_zero_set();
+		n = 3;
+		break;
 	case DSF60_COMMAND_READ_ENCODER_TYPE:
 		DSF60_read_encoder_type();
+		n = 37;
+		break;
+	case DSF60_COMMAND_SET_ZERO_PULSE_WIDTH_MECHANICAL:
+		DSF60_set_zero_pulse_width_mechanical((uint16_t) arg);
+		n = 3;
+		break;
+	case DSF60_COMMAND_READ_ZERO_PULSE_WIDTH_ELECTRIAL_MECHANICAL:
+		DSF60_read_zero_pulse_width_electrical_mechanical();
+		n = 5;
+		break;
+	case DSF60_COMMAND_READ_ELECTRICAL_INTERFACE:
+		DSF60_read_electrical_interface();
+		n = 4;
+		break;
+	default:
+		n = 0;
 		break;
 	}
+
+	delay_ms(100);
+
+	if (buffer_index != n) {
+		return false;
+	}
+
+	switch (command) {
+	case DSF60_COMMAND_READ_SERIAL_NUMBER:
+		for (cont = 0; cont < 9; ++cont)
+			dsf60->serial_number[cont] = buffer[cont + 2];
+
+		dsf60->serial_number[cont] = '\0';
+
+		for (cont = 0; cont < 10; ++cont)
+			dsf60->firmware_version[cont] = buffer[cont + 11];
+
+		dsf60->firmware_version[cont] = '\0';
+
+		for (cont = 0; cont < 8; ++cont)
+			dsf60->firmware_date[cont] = buffer[cont + 21];
+
+		dsf60->firmware_date[cont] = '\0';
+		break;
+	case DSF60_COMMAND_READ_POSITION:
+		dsf60->position = make32(buffer[2], buffer[3], buffer[4], buffer[5]);
+		break;
+	case DSF60_COMMAND_SET_NUMBER_OF_LINES:
+		break;
+	case DSF60_COMMAND_SET_ELECTRICAL_INTERFACE:
+		break;
+	case DSF60_COMMAND_SET_ZERO_PULSE_WIDTH_ELECTRICAL:
+		break;
+	case DSF60_COMMAND_ZERO_SET:
+		break;
+	case DSF60_COMMAND_READ_ENCODER_TYPE:
+		for (cont = 0; cont < 16; ++cont)
+			dsf60->part_number[cont] = buffer[cont + 2];
+
+		dsf60->part_number[cont] = '\0';
+
+		for (cont = 0; cont < 7; ++cont)
+			dsf60->encoder_type[cont] = buffer[cont + 18];
+
+		dsf60->encoder_type[cont] = '\0';
+
+		for (cont = 0; cont < 8; ++cont)
+			dsf60->date_code[cont] = buffer[cont + 25];
+
+		dsf60->date_code[cont] = '\0';
+		dsf60->resolution = make32(0, buffer[cont + 1], buffer[cont + 2],
+				buffer[cont + 3]);
+		break;
+	case DSF60_COMMAND_SET_ZERO_PULSE_WIDTH_MECHANICAL:
+		break;
+	case DSF60_COMMAND_READ_ZERO_PULSE_WIDTH_ELECTRIAL_MECHANICAL:
+		pulse_width_resp = make16(buffer[2], buffer[3]);
+
+		if (pulse_width_resp > 1 && pulse_width_resp < 360) {
+			dsf60->pulse_width_mechanical = pulse_width_resp;
+			dsf60->pulse_width_electrical = 0;
+		} else {
+			dsf60->pulse_width_mechanical = 0;
+			dsf60->pulse_width_electrical &= 0x00FF;
+
+			if ((pulse_width_resp & DSF60_90_DEGREES) == DSF60_90_DEGREES)
+				dsf60->pulse_width_electrical = DSF60_90_DEGREES;
+			else if ((pulse_width_resp & DSF60_180_DEGREES_CHANNEL_A)
+					== DSF60_180_DEGREES_CHANNEL_A)
+				dsf60->pulse_width_electrical = DSF60_180_DEGREES_CHANNEL_A;
+			else if ((pulse_width_resp & DSF60_180_DEGREES_CHANNEL_B)
+					== DSF60_180_DEGREES_CHANNEL_B)
+				dsf60->pulse_width_electrical = DSF60_180_DEGREES_CHANNEL_B;
+			else if ((pulse_width_resp & DSF60_270_DEGREES)
+					== DSF60_270_DEGREES)
+				dsf60->pulse_width_electrical = DSF60_270_DEGREES;
+		}
+
+		break;
+	case DSF60_COMMAND_READ_ELECTRICAL_INTERFACE:
+		if ((buffer[2] & 0b0001) == DSF60_TTL)
+			dsf60->pulse_width_electrical = DSF60_TTL;
+		else
+			dsf60->pulse_width_electrical = DSF60_HTL;
+
+		break;
+	}
+
+	DSF60_flush_buffer();
 
 	return true;
 }
--- src/sick.h
+++ src/sick.h
@@ -8,11 +8,42 @@
 #ifndef SICK_H_
 #define SICK_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #define EXIT_SUCESS		0
 #define EXIT_ERROR		0xFF
 #define DSF60_ADDRESS	0x40
 #define BUFFER_MAX		0x80
 #define ENCODER_EN		PIN_C2
+
+typedef enum electrical_interface_e {
+	DSF60_TTL, DSF60_HTL
+} electrical_interface_t;
+
+typedef enum pulse_width_electrical_e {
+	DSF60_90_DEGREES,
+	DSF60_180_DEGREES_CHANNEL_B,
+	DSF60_180_DEGREES_CHANNEL_A,
+	DSF60_270_DEGREES
+} pulse_width_electrical_t;
+
+typedef enum pulse_width_mechanical_e {
+	DSF60_45_DEGREES_MECHANICAL, DSF60_270_DEGREES_MECHANICAL,
+} pulse_width_mechanical_t;
+
+typedef struct dsf60_str {
+	pulse_width_electrical_t pulse_width_electrical;
+	char serial_number[10];
+	char firmware_version[11];
+	char firmware_date[9];
+	char part_number[17];
+	char encoder_type[8];
+	char date_code[9];
+	uint32_t position;
+	uint32_t resolution;
+	uint16_t pulse_width_mechanical;
+} dsf60_t;
 
 typedef enum dsf60_command_e {
 	DSF60_COMMAND_ENCODER_RESET = 0x53,
@@ -27,5 +58,7 @@ typedef enum dsf60_command_e {
 	DSF60_COMMAND_READ_ZERO_PULSE_WIDTH_ELECTRIAL_MECHANICAL,
 	DSF60_COMMAND_READ_ELECTRICAL_INTERFACE
 } DSF60_command_t;
+
+dsf60_t *dsf60;
 
 #endif /* SICK_H_ */
